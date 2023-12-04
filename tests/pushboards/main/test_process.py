fa3174d31a0e1525340c40890283b962d9eaf959
Viacheslav Matrizaev <matrizaev@gmail.com>
2023-12-04 23:28:35 +0300

    added unit test for update_value_list_dict

---
+++ tests/pushboards/main/test_process.py
@@ -0,0 +1,24 @@
+from pushboards.main.process import update_value_list_dict
+
+
+def test_update_value_list_dict():
+    value_list_dict = {
+        "key1": ["value1", "value2"],
+        "key2": ["value3", "value4"],
+    }
+    update_value_list_dict(value_list_dict, "key1", "value5")
+    assert value_list_dict == {
+        "key1": ["value1", "value2", "value5"],
+        "key2": ["value3", "value4"],
+    }
+    update_value_list_dict(value_list_dict, "key1", "value6")
+    assert value_list_dict == {
+        "key1": ["value1", "value2", "value5", "value6"],
+        "key2": ["value3", "value4"],
+    }
+    update_value_list_dict(value_list_dict, "key3", "value7")
+    assert value_list_dict == {
+        "key1": ["value1", "value2", "value5", "value6"],
+        "key2": ["value3", "value4", ""],
+        "key3": ["", "", "", "value7"],
+    }
