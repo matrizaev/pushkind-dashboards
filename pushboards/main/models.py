ae9e999ed892096102c36ecfd63f442930c14864
Viacheslav Matrizaev <matrizaev@gmail.com>
2023-12-07 00:38:04 +0300

    ajax delete files

--- pushboards/main/models.py
+++ pushboards/main/models.py
@@ -1,4 +1,9 @@
-from pushboards.auth.models import User
+from pathlib import Path
+from uuid import uuid4
+
+from flask import current_app, url_for
+from werkzeug.datastructures import FileStorage
+
 from pushboards.extensions import db
 
 
@@ -13,11 +18,19 @@ class UserFile(db.Model):  # noqa: R0401, R0903
 
     user = db.relationship("User", backref=db.backref("files", lazy=True))
 
-    def __init__(self, file_name: str, file_url: str, file_path: str, user: User):
-        self.file_name = file_name
-        self.file_url = file_url
-        self.file_path = file_path
-        self.user = user
+    def __init__(self, file_post: FileStorage, user_id: int, process_fn: callable):
+        file_path = Path(current_app.config["STATIC_UPLOAD_PATH"])
+        file_path = file_path / uuid4().hex
+        file_path = file_path.with_suffix(current_app.config["FILE_UPLOAD_SUFFIX"])
+        file_path = Path(current_app.static_folder) / file_path
+
+        file_data = process_fn(file_post.stream)
+        file_path.write_bytes(file_data)
+
+        self.file_path = str(file_path)
+        self.file_name = file_post.filename
+        self.user_id = user_id
+        self.file_url = url_for("static", filename=file_path.relative_to(current_app.static_folder))
 
     def to_json(self):
         return {
@@ -27,6 +40,3 @@ class UserFile(db.Model):  # noqa: R0401, R0903
             "file_path": self.file_path,
             "user_id": self.user_id,
         }
-
-    def process(self):
-        pass
--- pushboards/main/routes.py
+++ pushboards/main/routes.py
@@ -1,7 +1,6 @@
 from pathlib import Path
-from uuid import uuid4
 
-from flask import Blueprint, abort, current_app, flash, jsonify, redirect, render_template, request, url_for
+from flask import Blueprint, abort, jsonify, render_template, request
 from flask_login import current_user, login_required
 
 from pushboards.extensions import db
@@ -9,10 +8,6 @@ from pushboards.main.models import UserFile
 from pushboards.main.process import process
 
 bp = Blueprint("main", __name__)
-
-
-def wants_json_response():
-    return request.accept_mimetypes["application/json"] >= request.accept_mimetypes["text/html"]
 
 
 @bp.route("/")
@@ -26,15 +21,12 @@ def index():
 @login_required
 def upload():
     file_post = request.files["file"]
-    file_name = (Path(current_app.config["STATIC_UPLOAD_PATH"]) / f"{uuid4().hex}").with_suffix(".xlsx")
-    file_path = Path(current_app.static_folder) / file_name
-    file_url = url_for("static", filename=file_name)
-    file_data = process(file_post.stream)
-    file_path.write_bytes(file_data)
-    file = UserFile(file_name=file_post.filename, file_url=file_url, file_path=str(file_path), user=current_user)
-    db.session.add(file)
+    user_file = UserFile(file_post=file_post, user_id=current_user.id, process_fn=process)
+    db.session.add(user_file)
     db.session.commit()
-    return jsonify({"status": "ok", "file_url": file_url, "file_name": file_post.filename, "id": file.id})
+    return jsonify(
+        {"status": "ok", "file_url": user_file.file_url, "file_name": user_file.file_name, "id": user_file.id}
+    )
 
 
 @bp.route("/remove/<int:file_id>", methods=["POST"])
@@ -45,10 +37,6 @@ def remove(file_id):
         Path(file.file_path).unlink(missing_ok=True)
         db.session.delete(file)
         db.session.commit()
-        flash("Файл удален", "success")
     else:
         abort(404)
-    if not wants_json_response():
-        return redirect(url_for("main.index"))
-
     return jsonify({"status": "ok"})
