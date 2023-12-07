03293f1bf3b86f128bc011fca510919d18de9eab
Viacheslav Matrizaev <matrizaev@gmail.com>
2023-12-07 13:39:14 +0300

    error pages

--- pushboards/__init__.py
+++ pushboards/__init__.py
@@ -53,9 +53,9 @@ def register_errorhandlers(app):
         """Render error template."""
         # If a HTTPException, pull the `code` attribute; default to 500
         error_code = getattr(error, "code", 500)
-        return render_template(f"errors/{error_code}.html"), error_code
+        return render_template(f"errors/{error_code}.html", error=error), error_code
 
-    for errcode in [401, 404, 500]:
+    for errcode in [400, 401, 403, 404, 413, 500, 503]:
         app.errorhandler(errcode)(render_error)
 
 
